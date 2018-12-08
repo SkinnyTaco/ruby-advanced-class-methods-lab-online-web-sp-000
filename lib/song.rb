ba027a9f5f97722920ef9e4ae4b5bda08b8aec1a
Daniel Dawson <dannyldawson@gmail.com>
2018-12-08 19:48:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,41 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    song = self.new 
+    song.save
+    song
+  end
+  
+  def self.new_by_name(song_name)
+    song = self.new 
+    song.name = song_name
+    song
+  end
+  
+  def self.create_by_name(song_name)
+    song = self.new_by_name(song_name)
+    song.save
+    song
+  end
+  
+  def self.find_by_name(song_name)
+    self.all.detect { |song| song.name == song_name }
+  end
+  
+  def self.find_or_create_by_name(song_name)
+    song = self.find_by_name(song_name)
+    song ? song : self.create_by_name(song_name)
+  end
+  
+  def self.alphabetical
+    self.all.sort_by { |song| song.name }
+  end
+  
+  def self.new_from_filename(song_file)
+    file_array = song_file.split(" - ")
+    song_name = 
+  end
 
 end
